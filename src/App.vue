60483355b5645049878709734c2b8c8113e27024
chichoon <chichoon.choi@gmail.com>
2023-10-17 08:56:47 +0900

    feat: :sparkles: Footer 추가

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router';
-import NavHeader from './components/NavHeader/NavHeader.vue';
-</script>
-
 <template>
   <NavHeader />
   <RouterView />
+  <FooterComponent />
 </template>
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+import NavHeader from './components/NavHeader/NavHeader.vue';
+import FooterComponent from './components/FooterComponent.vue';
+</script>
 
 <style lang="scss">
 @import '@/assets/colors.scss';
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer>
+    <div class="container">
+      <RouterLink to="/" class="logo-font">conduit</RouterLink>
+      <span class="attribution">
+        An interactive learning project from <a href="https://thinkster.io">Thinkster</a>. Code &amp; design licensed
+        under MIT.
+      </span>
+    </div>
+  </footer>
+</template>
