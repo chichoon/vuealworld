f6f0ff24db778f0c39a5c8fde36475bdfae1ecbd
chichoon <chichoon.choi@gmail.com>
2023-10-17 08:58:13 +0900

    feat: :lipstick: 헤더 디자인 추가

--- src/components/NavHeader/NavHeader.vue
+++ src/components/NavHeader/NavHeader.vue
@@ -1,12 +1,19 @@
 <template>
-  <nav class="header">
-    <RouterLink to="/">
-      <h1>conduit</h1>
-    </RouterLink>
-    <div class="navigate">
-      <RouterLink to="/" class="link" active-class="selected">home</RouterLink>
-      <RouterLink to="/signin" class="link" active-class="selected">Sign in</RouterLink>
-      <RouterLink to="/signup" class="link" active-class="selected">Sign up</RouterLink>
+  <nav class="navbar navbar-light">
+    <div class="container">
+      <RouterLink class="navbar-brand" to="/">conduit</RouterLink>
+      <ul class="nav navbar-nav pull-xs-right">
+        <li class="nav-item">
+          <!-- Add "active" class when you're on that page" -->
+          <RouterLink class="nav-link" active-class="active" to="/">Home</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink class="nav-link" active-class="active" to="/login">Sign in</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink class="nav-link" active-class="active" to="/register">Sign up</RouterLink>
+        </li>
+      </ul>
     </div>
   </nav>
 </template>
@@ -15,41 +22,4 @@
 console.log(window.location.pathname);
 </script>
 
-<style lang="scss" scoped>
-.header {
-  width: 100%;
-  padding: 20px 10%;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-
-  & > a {
-    text-decoration: none;
-    color: #000000;
-
-    h1 {
-      font-size: 30px;
-    }
-  }
-}
-
-.navigate {
-  width: 200px;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-}
-
-.link {
-  text-decoration: none;
-  background: none;
-  border: none;
-  color: #aaaaaa;
-
-  &.selected {
-    color: #000000;
-  }
-}
-</style>
+<style lang="scss" scoped></style>
