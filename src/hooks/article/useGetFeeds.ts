6c7d27b198f13808385f7a6335a247b100084fe5
chichoon <chichoon.choi@gmail.com>
2023-10-19 08:49:34 +0900

    feat: :label: useGetArticles, useGetFeeds 타이핑

--- src/hooks/article/useGetFeeds.ts
+++ src/hooks/article/useGetFeeds.ts
@@ -3,8 +3,8 @@ import { useQuery } from '@tanstack/vue-query';
 import articles from '@/services/articles';
 import type { ArticlesResponse } from '@/types/article';
 
-export function useGetArticles(page: number) {
-  return useQuery<ArticlesResponse>(['articles', page], () => articles.get({ offset: page * 10 - 10, limit: 10 }), {
+export function useGetFeeds(page: number) {
+  return useQuery<ArticlesResponse>(['feeds', page], () => articles.getFeed({ offset: page * 10 - 10, limit: 10 }), {
     keepPreviousData: true,
   });
 }
