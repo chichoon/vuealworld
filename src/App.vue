209f81500ec07afdf39dcf0ee1e3ce04d7c192ae
chichoon <chichoon.choi@gmail.com>
2023-10-18 10:35:27 +0900

    style: :fire: 불필요해보이는 코드 삭제

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,18 @@
 <template>
-  <AuthenticatedNavHeader v-if="isAuthenticated" />
+  <AuthenticatedNavHeader v-if="!!data" :user-info="data.user" />
   <NavHeader v-else />
   <RouterView />
   <FooterComponent />
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue';
 import { RouterView } from 'vue-router';
 
 import { NavHeader, AuthenticatedNavHeader } from './components/NavHeader';
 import FooterComponent from './components/FooterComponent.vue';
-import { useGetUserData } from './hooks/user/useGetUserData';
+import { useGetCurrentUserData } from './hooks/user/useGetCurrentUserData';
 
-const isAuthenticated = ref<boolean>(false);
-
-const data = useGetUserData();
-watch(data, () => {
-  console.log(data.value.user);
-  isAuthenticated.value = !!data;
-});
+const data = useGetCurrentUserData();
 </script>
 
 <style lang="scss"></style>
