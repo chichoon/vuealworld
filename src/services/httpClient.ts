45d6f80e9ee7f8ce838c128d0c2765060a19f9b2
chichoon <chichoon.choi@gmail.com>
2023-10-18 16:22:34 +0900

    feat: :sparkles: params 받아서 query string 으로 변경하는 함수 추가

--- src/services/httpClient.ts
+++ src/services/httpClient.ts
@@ -1,14 +1,17 @@
 import { useCookies } from 'vue3-cookies';
 
+import createURLParams from '@/utils/createURLParams';
+
 interface Params {
   url: string;
   method: string;
+  params?: Record<string, string | number | undefined>;
   body?: object;
 }
 
-export async function httpClient({ url, method, body }: Params) {
+export async function httpClient({ url, method, params, body }: Params) {
   const { cookies } = useCookies();
-  return await fetch('/api' + url, {
+  return await fetch('/api' + createURLParams(url, params), {
     method,
     headers: {
       'Content-Type': 'application/json',
---
+++ src/utils/createURLParams.ts
@@ -0,0 +1,13 @@
+function returnString(value: string | number) {
+  if (typeof value === 'number') return value.toString();
+  return value;
+}
+
+export default function createURLParams(url: string, object?: Record<string, string | number | undefined>): string {
+  if (!object) return url;
+  const newObj: Record<string, string> = {};
+  Object.keys(object).forEach((key) => object[key] && (newObj[key] = returnString(object[key] as string | number)));
+  const paramsString = new URLSearchParams(newObj).toString();
+
+  return `${url}?${paramsString}`;
+}
