28b0506d07e82eb8c85babe42069841f61310311
chichoon <chichoon.choi@gmail.com>
2023-10-18 15:34:00 +0900

    feat: :sparkles: 게시글 작성 폼 및 태그 폼 추가

---
+++ src/components/CustomForm/EditorForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <CustomInput type="text" class="form-control form-control-lg" placeholder="Article Title" v-model:value="title" />
+    <fieldset class="form-group"></fieldset>
+    <fieldset class="form-group">
+      <CustomInput
+        type="text"
+        class="form-control"
+        placeholder="What's this article about?"
+        v-model:value="description"
+      />
+    </fieldset>
+    <CustomInput
+      is-text-area
+      class="form-control"
+      rows="8"
+      placeholder="Write your article (in markdown)"
+      v-model:value="body"
+    ></CustomInput>
+    <fieldset class="form-group"></fieldset>
+    <TagForm v-model:tags="tagList" />
+    <button class="btn btn-lg pull-xs-right btn-primary" type="button">Publish Article</button>
+  </form>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+import CustomInput from './CustomInput.vue';
+import TagForm from './TagForm.vue';
+
+const title = ref('');
+const description = ref('');
+const body = ref('');
+const tagList = ref<string[]>([]);
+
+function handleSubmit(e: Event) {
+  console.log(e);
+}
+</script>
