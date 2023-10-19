ca4a7b5a2c1fc730a1a54c3d604fefde757b926a
chichoon <chichoon.choi@gmail.com>
2023-10-19 17:39:26 +0900

    fix: :bug: routerview 강제 리렌러

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
   <AuthenticatedNavHeader v-if="!!data" :user-info="data" />
   <NavHeader v-else />
-  <RouterView />
+  <RouterView :key="route.fullPath" />
   <FooterComponent />
 </template>
 
 <script setup lang="ts">
-import { RouterView } from 'vue-router';
+import { RouterView, useRoute } from 'vue-router';
 
 import { NavHeader, AuthenticatedNavHeader } from './components/NavHeader';
 import FooterComponent from './components/FooterComponent.vue';
 import { useGetCurrentUserData } from './hooks/user';
 
 const { data } = useGetCurrentUserData();
+const route = useRoute();
 </script>
 
 <style lang="scss"></style>
