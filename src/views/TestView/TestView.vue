8c4c96bf379bec53d04e1eb30f172ed9c1b254c0
chichoon <chichoon.choi@gmail.com>
2023-10-20 13:58:29 +0900

    feat: :sparkles: 비밀의 테스트 공간 추가

---
+++ src/views/TestView/TestView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Welcome to the secret test page!!</h1>
+  </div>
+  <div>
+    <VBindTest v-model:value1="input1" v-model:value2="input2" />
+    <span>input 1: {{ input1 }}</span>
+    <br />
+    <span>input 2: {{ input2 }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import VBindTest from './VBindTest.vue';
+
+const input1 = ref('');
+const input2 = ref('');
+</script>
