1b6e3c395551a65a95b856dfc6ea763a740eaaf3
chichoon <chichoon.choi@gmail.com>
2023-10-21 14:15:15 +0900

    feat: :sparkles: Tag 부분 Query Key Factory 적용

---
+++ src/hooks/tag/queries.ts
@@ -0,0 +1,3 @@
+export const tagKeys = {
+  root: ['tags'] as const,
+};
--- src/hooks/tag/useGetTags.ts
+++ src/hooks/tag/useGetTags.ts
@@ -2,9 +2,12 @@ import { useQuery } from '@tanstack/vue-query';
 
 import tag from '@/services/tag';
 import { CACHE_TIME, STALE_TIME } from '@/utils/constants';
+import { tagKeys } from './queries';
 
 export function useGetTags() {
-  return useQuery(['tags'], () => tag.get(), {
+  return useQuery({
+    queryKey: tagKeys.root,
+    queryFn: () => tag.get(),
     cacheTime: CACHE_TIME,
     staleTime: STALE_TIME,
   });
