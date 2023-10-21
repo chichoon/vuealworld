04152c249cde2c7e19f3129d76760c1f8ccc73b2
chichoon <chichoon.choi@gmail.com>
2023-10-21 20:08:01 +0900

    feat: :poop: 만악의 근원을 지워버림 (like article 을 Preview에서 삭제

--- src/components/ArticlePreview.vue
+++ src/components/ArticlePreview.vue
@@ -15,14 +15,6 @@
         }}</RouterLink>
         <span class="date">{{ new Date(articleInfo.createdAt).toDateString() }}</span>
       </div>
-      <button
-        @click="handleClickFavorite"
-        class="btn btn-outline-primary btn-sm pull-xs-right"
-        :class="{ favorited: articleInfo.favorited }"
-      >
-        <i class="ion-heart"></i>
-        <span> {{ articleInfo.favoritesCount }}</span>
-      </button>
     </div>
     <RouterLink :to="`/article/${articleInfo.slug}`" class="preview-link">
       <h1>{{ articleInfo.title }}</h1>
@@ -42,9 +34,6 @@
 </template>
 
 <script lang="ts" setup>
-import { useQueryClient } from '@tanstack/vue-query';
-
-import { usePostFavorite, useDeleteFavorite } from '@/hooks/article';
 import type { ArticleData } from '@/types/article';
 
 interface Props {
@@ -52,18 +41,6 @@ interface Props {
 }
 
 const { articleInfo } = defineProps<Props>();
-const queryClient = useQueryClient();
-
-const { mutate: favoriteMutate } = usePostFavorite(queryClient, articleInfo.slug);
-const { mutate: unfavoriteMutate } = useDeleteFavorite(queryClient, articleInfo.slug);
-
-function handleClickFavorite() {
-  if (articleInfo.favorited) {
-    unfavoriteMutate();
-  } else {
-    favoriteMutate();
-  }
-}
 </script>
 
 <style scoped>
