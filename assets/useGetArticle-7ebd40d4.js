6274ab51b90ee8695347a00c9737ce9eeb19d484
chichoon <chichoon.choi@gmail.com>
2023-10-24 14:18:07 +0900

    deploy

---
+++ assets/useGetArticle-7ebd40d4.js
@@ -0,0 +1,1 @@
+import{B as a,n as r,m as s,C as t,S as u}from"./index-896a6a3c.js";function c(e){return a({queryKey:r.article.slug(e),queryFn:()=>s.getBySlug(e.value),cacheTime:t,staleTime:u})}export{c as u};
