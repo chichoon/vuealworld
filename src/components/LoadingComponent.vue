c0100263fc79028e799fb02efe4a545a43eeacf9
chichoon <chichoon.choi@gmail.com>
2023-10-26 09:17:09 +0900

    feat: :lipstick: 로딩 컴포넌트 개선

--- src/components/LoadingComponent.vue
+++ src/components/LoadingComponent.vue
@@ -1,3 +1,34 @@
 <template>
-  <h1>Loading...</h1>
+  <div class="loading-wrapper">
+    <LoadingIcon class="loading-icon" />
+  </div>
 </template>
+
+<script setup lang="ts">
+import LoadingIcon from './icons/LoadingIcon.vue';
+</script>
+
+<style scoped>
+@keyframes loadingSpin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.loading-wrapper {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 20px 0;
+}
+
+.loading-icon {
+  width: 40px;
+  height: 40px;
+  animation: loadingSpin 1s linear infinite;
+}
+</style>
