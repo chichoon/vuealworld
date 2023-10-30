f3c405be3023a39d4d3f4d6e2f1ff12ee2150e99
chichoon <chichoon.choi@gmail.com>
2023-10-30 10:52:04 +0900

    refactor: :recycle: port 정의

--- src/01_application/ports/user.d.ts
+++ src/01_application/ports/user.d.ts
@@ -1,14 +1,3 @@
-export interface SignUpData {
-  username: string;
-  email: string;
-  password: string;
-}
-
-export interface SignInData {
-  email: string;
-  password: string;
-}
-
 export interface EditUserData {
   email?: string;
   password?: string;
