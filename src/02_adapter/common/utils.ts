df510f5a8b838f528b7af1e142af35e9c0e41e75
chichoon <chichoon.choi@gmail.com>
2023-10-30 10:51:51 +0900

    refactor: :recycle: 상수, 유틸함수 common 폴더로 이동

--- src/02_adapter/common/utils.ts
+++ src/02_adapter/common/utils.ts
@@ -1,9 +1,17 @@
+export function concatErrorMessage(errorObj: Record<string, string>): string {
+  const keys = Object.keys(errorObj);
+
+  return keys.reduce((acc, cur) => {
+    return acc + `\n${cur}: ${errorObj[cur]}`;
+  }, '');
+}
+
 function returnString(value: string | number) {
   if (typeof value === 'number') return value.toString();
   return value;
 }
 
-export default function createURLParams(url: string, object?: Record<string, string | number | undefined>): string {
+export function createURLParams(url: string, object?: Record<string, string | number | undefined>): string {
   if (!object) return url;
   const newObj: Record<string, string> = {};
   Object.keys(object).forEach((key) => object[key] && (newObj[key] = returnString(object[key] as string | number)));
