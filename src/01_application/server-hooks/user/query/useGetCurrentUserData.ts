9d4aae16e332aa2c47788882b014e19affd0c87d
chichoon <chichoon.choi@gmail.com>
2023-10-30 10:54:08 +0900

    chore: :art: 불러오기 이슈 수정

--- src/01_application/server-hooks/user/query/useGetCurrentUserData.ts
+++ src/01_application/server-hooks/user/query/useGetCurrentUserData.ts
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/vue-query';
 
-import user from '@/services/user';
+import user from '@/02_adapter/services/user';
 import type { UserData } from '@/00_domain/entity/user';
 import { CACHE_TIME, STALE_TIME } from '@/01_application/common/constants';
 import { userKeys } from '../query-key';
