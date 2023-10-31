145cb3c3d61e5adf3ebbb6a892da94c32627e75a
chichoon <chichoon.choi@gmail.com>
2023-10-31 09:00:41 +0900

    test: :white_check_mark: 피드 체크 함수 하나로 합침

--- tests/article/article-list.spec.ts
+++ tests/article/article-list.spec.ts
@@ -1,24 +1,30 @@
-import { expect, test } from '@playwright/test';
+import { Page, expect, test } from '@playwright/test';
 import { login } from '../utils/login';
+
+async function testArticlesCountFromPath(page: Page, path: string) {
+  const articlesRequest = await page.waitForResponse((res) => res.url().endsWith(`${path}?limit=10`));
+  const articlesResponse = await articlesRequest.json();
+  expect(articlesResponse).toHaveProperty('articlesCount');
+
+  const articlesCountFromResponse = articlesResponse.articlesCount;
+  const paginationList = await page.getByTestId('pagination-button').all();
+  const pageButton = paginationList[paginationList.length - 1];
+  await pageButton.click();
+
+  await page.waitForResponse((res) =>
+    res.url().endsWith(`${path}?offset=${(paginationList.length - 1) * 10}&limit=10`),
+  );
+  await page.waitForTimeout(2000);
+  const articlesCount = (await page.getByTestId('article-preview').count()) + (paginationList.length - 1) * 10;
+  expect(articlesCount).toEqual(articlesCountFromResponse);
+}
 
 test('User Feed test', async ({ page }) => {
   await login(page);
   await page.goto('/');
   await page.waitForSelector('text=Your Feed');
 
-  const myFeedArticlesRequest = await page.waitForResponse((res) => res.url().endsWith('/feed?limit=10'));
-  const myFeedArticlesResponse = await myFeedArticlesRequest.json();
-  expect(myFeedArticlesResponse).toHaveProperty('articlesCount');
-
-  const articlesCountFromResponse = myFeedArticlesResponse.articlesCount;
-  const paginationList = await page.getByTestId('pagination-button').all();
-  const pageButton = paginationList[paginationList.length - 1];
-  await pageButton.click();
-
-  await page.waitForResponse((res) => res.url().endsWith(`/feed?offset=${(paginationList.length - 1) * 10}&limit=10`));
-  await page.waitForTimeout(2000);
-  const articlesCount = (await page.getByTestId('article-preview').count()) + (paginationList.length - 1) * 10;
-  expect(articlesCount).toEqual(articlesCountFromResponse);
+  await testArticlesCountFromPath(page, '/feed');
 });
 
 test('Global Feed test', async ({ page }) => {
@@ -26,20 +32,5 @@ test('Global Feed test', async ({ page }) => {
   await page.goto('/');
   await page.waitForSelector('text=Global Feed');
 
-  await page.getByRole('button', { name: 'Global Feed' }).click();
-  const globalFeedArticlesRequest = await page.waitForResponse((res) => res.url().endsWith('/articles?limit=10'));
-  const globalFeedArticlesResponse = await globalFeedArticlesRequest.json();
-  expect(globalFeedArticlesResponse).toHaveProperty('articlesCount');
-
-  const articlesCountFroResponse = globalFeedArticlesResponse.articlesCount;
-  const paginationList = await page.getByTestId('pagination-button').all();
-  const pageButton = paginationList[paginationList.length - 1];
-  await pageButton.click();
-
-  await page.waitForResponse((res) =>
-    res.url().endsWith(`/articles?offset=${(paginationList.length - 1) * 10}&limit=10`),
-  );
-  await page.waitForTimeout(2000);
-  const articlesCount = (await page.getByTestId('article-preview').count()) + (paginationList.length - 1) * 10;
-  expect(articlesCount).toEqual(articlesCountFroResponse);
+  await testArticlesCountFromPath(page, '/articles');
 });
