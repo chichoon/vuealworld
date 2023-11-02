a56ab9d0a25560d8e4638cde17f0c3832e5feedf
chichoon <chichoon.choi@gmail.com>
2023-11-02 11:53:56 +0900

    test: :white_check_mark: 게시글 CRUD 테스트 추가

--- tests/article/article-crud.spec.ts
+++ tests/article/article-crud.spec.ts
@@ -1,35 +1,62 @@
 import { test, expect } from '@playwright/test';
 import { login } from '../utils/login';
 
-const title = `${Math.floor(Math.random() * 1000000)}Article`;
-
-test('Article Creation', async ({ page }) => {
+test('Article CRUD', async ({ page }) => {
+  const title = `${Math.floor(Math.random() * 1000000)}Article`;
   await login(page);
 
-  await page.getByRole('link', { name: 'New Article' }).click();
-  await page.waitForURL('/editor');
+  await test.step('Create Article', async () => {
+    await page.getByRole('link', { name: 'New Article' }).click();
+    await page.waitForURL('/editor');
 
-  await page.getByPlaceholder('Article Title').fill(title);
-  await page.getByPlaceholder("What's this article about?").fill('Test description');
-  await page.getByPlaceholder('Write your article (in markdown)').fill('Test body');
-  await page.getByPlaceholder('Enter tags').fill('test');
-  await page.keyboard.press('Enter');
-  await expect(page.getByRole('button', { name: 'test' })).toBeVisible();
+    await page.getByPlaceholder('Article Title').fill(title);
+    await page.getByPlaceholder("What's this article about?").fill('Test description');
+    await page.getByPlaceholder('Write your article (in markdown)').fill('Test body');
+    await page.getByPlaceholder('Enter tags').fill('test');
+    await page.keyboard.press('Enter');
+    await expect(page.getByRole('button', { name: 'test' })).toBeVisible();
 
-  await page.getByRole('button', { name: 'Publish Article' }).click();
-  await page.waitForURL('/');
+    await page.getByRole('button', { name: 'Publish Article' }).click();
+    await page.waitForURL('/');
 
-  await page.getByRole('button', { name: 'Global Feed' }).click();
-  await page.locator('.article-preview').first().waitFor({ state: 'visible' });
-  const article = page.getByTestId('article-preview').first();
-  expect(article.getByRole('heading', { name: title })).toBeVisible();
-});
+    await page.getByRole('button', { name: 'Global Feed' }).click();
+    const article = page.locator('.article-preview').first();
+    await article.waitFor({ state: 'visible' });
+    await expect(article.getByRole('heading', { name: title })).toBeVisible();
+  });
 
-test('Article Read', async ({ page }) => {
-  await page.goto(`/article/${title}-${process.env.ID}`);
-  await page.locator('.container > h1').first().waitFor({ state: 'visible' });
-  expect(page.getByRole('heading', { name: title })).toBeVisible();
-  expect(page.locator('.author').first()).toBeVisible();
-  expect(page.locator('.col-md-12 > p')).toBeVisible();
-  expect(page.locator('.tag-default')).toBeVisible();
+  await test.step('Read Article', async () => {
+    await page.goto(`/article/${title}-${process.env.ID}`);
+    await page.locator('.container > h1').first().waitFor({ state: 'visible' });
+    await expect(page.getByRole('heading', { name: title })).toBeVisible();
+    await expect(page.locator('.author').first()).toBeVisible();
+    await expect(page.locator('.col-md-12 > p')).toBeVisible();
+    await expect(page.locator('.tag-default')).toBeVisible();
+  });
+
+  await test.step('Update Article', async () => {
+    await page.getByRole('link', { name: 'Edit Article' }).first().click();
+    await expect(page).toHaveURL(`/editor/${title}-${process.env.ID}`);
+    await page.locator('.form-control').nth(1).fill('Test description updated');
+    await page.locator('.form-control').nth(2).fill('Test body updated');
+
+    await page.getByRole('button', { name: 'Edit Article' }).click();
+    await page.locator('.container > h1').first().waitFor({ state: 'visible' });
+
+    await expect(page.getByRole('heading', { name: title })).toBeVisible();
+    await expect(page.locator('.col-md-12 > p')).toHaveText('Test body updated');
+  });
+
+  await test.step('Delete Article', async () => {
+    page.on('dialog', async (dialog) => {
+      expect(dialog.message()).toContain('Are you sure to delete this article?');
+      await dialog.accept();
+    });
+
+    await page.getByRole('button', { name: 'Delete Article' }).first().click();
+    await expect(page).toHaveURL('/');
+    await page.goto(`/article/${title}-${process.env.ID}`);
+    await page.waitForTimeout(5000);
+    await expect(page.locator('.container > h1').first()).not.toBeVisible();
+  });
 });
