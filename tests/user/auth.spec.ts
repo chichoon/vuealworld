0b73af29e0179f18f7963f42f4b55247ec5fa7ed
chichoon <chichoon.choi@gmail.com>
2023-11-02 11:54:13 +0900

    test: :white_check_mark: await 필요한 곳에 전부 추가

--- tests/user/auth.spec.ts
+++ tests/user/auth.spec.ts
@@ -1,42 +1,46 @@
 import { test, expect } from '@playwright/test';
 
-const id = `a${Math.random()}atest`;
+const id = `a${Math.floor(Math.random() * 100000)}atest`;
 
 test('User Register', async ({ page }) => {
   await page.goto('/register');
   await expect(page).toHaveURL('/register');
 
-  await page.getByPlaceholder('Username').fill(`${id}`);
+  await page.getByPlaceholder('Username').fill(id);
   await page.getByPlaceholder('Email').fill(`${id}@gmail.com`);
-  await page.getByPlaceholder('Password').fill(`${id}`);
+  await page.getByPlaceholder('Password').fill(id);
   await page.getByRole('button', { name: 'Sign up' }).click();
   await expect(page).toHaveURL('/');
 });
 
 test('User Login and Logout', async ({ page }) => {
-  await page.goto('/login');
-  await expect(page).toHaveURL('/login');
+  await test.step('Login', async () => {
+    await page.goto('/login');
+    await expect(page).toHaveURL('/login');
 
-  await page.getByPlaceholder('Email').fill(`${id}@gmail.com`);
-  await page.getByPlaceholder('Password').fill(`${id}`);
-  await page.getByRole('button', { name: 'Sign in' }).click();
-  await expect(page).toHaveURL('/');
+    await page.getByPlaceholder('Email').fill(`${id}@gmail.com`);
+    await page.getByPlaceholder('Password').fill(id);
+    await page.getByRole('button', { name: 'Sign in' }).click();
+    await expect(page).toHaveURL('/');
 
-  const headerInfo = page.getByTestId('nav-userdata');
-  await expect(headerInfo).toHaveText(`${id}`);
-
-  const settingsBtn = page.getByRole('link', { name: 'Settings' });
-  await expect(settingsBtn).toHaveAttribute('href', '/settings');
-
-  page.on('dialog', async (dialog) => {
-    expect(dialog.message()).toContain('Are you sure you want to logout?');
-    await dialog.accept();
+    const headerInfo = page.getByTestId('nav-userdata');
+    await expect(headerInfo).toHaveText(id);
   });
 
-  await settingsBtn.click();
-  await expect(page).toHaveURL('/settings');
+  await test.step('Logout', async () => {
+    const settingsBtn = page.getByRole('link', { name: 'Settings' });
+    await expect(settingsBtn).toHaveAttribute('href', '/settings');
 
-  await page.waitForSelector('.form-control');
+    page.on('dialog', async (dialog) => {
+      expect(dialog.message()).toContain('Are you sure you want to logout?');
+      await dialog.accept();
+    });
+
+    await settingsBtn.click();
+    await expect(page).toHaveURL('/settings');
+
+    await page.locator('.form-control').waitFor({ state: 'visible' });
+  });
   const logoutBtn = page.getByRole('button', { name: 'Or click here to logout.' });
   await logoutBtn.click();
 
